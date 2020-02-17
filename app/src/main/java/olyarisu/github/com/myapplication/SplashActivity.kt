82935cd49f15e7817aac81ba8e898d710996d405
Olga Palaznik <olyarisu@gmail.com>
2020-02-17 18:40:35 +0200

    feat: implement splash screen

---
+++ app/src/main/java/olyarisu/github/com/myapplication/SplashActivity.kt
@@ -0,0 +1,15 @@
+package olyarisu.github.com.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
